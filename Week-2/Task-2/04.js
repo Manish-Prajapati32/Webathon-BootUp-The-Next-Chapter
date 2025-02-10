da11c6e15885cb0842b67ab09c792c87f96d23c9
Manish-Prajapati32 <manishprajapati9563@gmail.com>
2025-02-10 23:10:00 +0530

    Add files via upload

---
+++ Week-2/Task-2/04.js
@@ -0,0 +1,12 @@
+function Square_and_Cube_0_to_10() {
+    let i;
+
+    for (i = 1;i <= 10;i++) {
+        console.log(`Square of ${i}² = ${i*i}`)
+    }
+    for (i = 1;i <= 10;i++) {
+        console.log(`Cube of ${i}³ = ${i*i*i}`)
+    }
+}
+
+Square_and_Cube_0_to_10()
